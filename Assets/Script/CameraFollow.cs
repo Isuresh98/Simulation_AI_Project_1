4535289e12056f4b3c7a479c032cd5799c6f2946
Indunil Suresh <102247770+Isuresh98@users.noreply.github.com>
2023-03-22 19:31:05 +0530

    all car simulation set and error update

---
+++ Assets/Script/CameraFollow.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform target;
+    [SerializeField] private float distance = 10f;
+    [SerializeField] private float height = 2f;
+    [SerializeField] private float rotationDamping = 3f;
+    [SerializeField] private float heightDamping = 2f;
+
+    private float currentRotationAngle;
+    private float currentHeight;
+
+    void LateUpdate()
+    {
+        float wantedRotationAngle = target.eulerAngles.y;
+        float wantedHeight = target.position.y + height;
+
+        currentRotationAngle = Mathf.LerpAngle(currentRotationAngle, wantedRotationAngle, rotationDamping * Time.deltaTime);
+        currentHeight = Mathf.Lerp(currentHeight, wantedHeight, heightDamping * Time.deltaTime);
+
+        Quaternion currentRotation = Quaternion.Euler(0, currentRotationAngle, 0);
+
+        Vector3 targetPosition = target.position - currentRotation * Vector3.forward * distance;
+        targetPosition.y = currentHeight;
+
+        transform.position = targetPosition;
+        transform.LookAt(target.position + Vector3.up * height);
+    }
+
+    public void SetTarget(Transform target)
+    {
+        this.target = target;
+    }
+}
---
+++ Assets/Script/CarController.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    [SerializeField] private float maxSpeed = 30f;
+    [SerializeField] private float acceleration = 10f;
+    [SerializeField] private float deceleration = 10f;
+    [SerializeField] private float brakePower = 10f;
+    [SerializeField] private float turnSpeed = 100f;
+
+    private Rigidbody rb;
+    private float inputHorizontal;
+    private float inputVertical;
+    private float currentSpeed;
+    private CameraFollow cameraFollow;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.centerOfMass = new Vector3(0f, -0.5f, 0f);
+        cameraFollow = Camera.main.GetComponent<CameraFollow>();
+        cameraFollow.SetTarget(transform);
+    }
+
+    void FixedUpdate()
+    {
+        inputHorizontal = Input.GetAxis("Horizontal");
+        inputVertical = Input.GetAxis("Vertical");
+
+        if (inputVertical > 0)
+        {
+            currentSpeed = Mathf.Clamp(currentSpeed + (acceleration * Time.fixedDeltaTime), 0f, maxSpeed);
+        }
+        else if (inputVertical < 0)
+        {
+            currentSpeed = Mathf.Clamp(currentSpeed - (deceleration * Time.fixedDeltaTime), -maxSpeed / 2f, 0f);
+        }
+        else
+        {
+            currentSpeed = Mathf.MoveTowards(currentSpeed, 0f, deceleration * Time.fixedDeltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            currentSpeed = Mathf.MoveTowards(currentSpeed, 0f, brakePower * Time.fixedDeltaTime);
+        }
+
+        Vector3 movement = transform.forward * currentSpeed * inputVertical;
+        rb.AddForce(movement, ForceMode.Acceleration);
+
+        float turn = inputHorizontal * turnSpeed * Time.fixedDeltaTime * Mathf.Lerp(1f, 0.2f, rb.velocity.magnitude / maxSpeed);
+        Quaternion turnRotation = Quaternion.Euler(0f, turn, 0f);
+        rb.MoveRotation(rb.rotation * turnRotation);
+    }
+}
